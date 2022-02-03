5a90b71da8b6054ade37a232a69e3b1bbccaba4d
Pavel Movchan <tz_95@mail.ru>
2022-02-03 13:10:12 +0900

    added 1st project

--- js/index.js
+++ js/index.js
@@ -1,84 +1,82 @@
-
-
 const parallax = (e) => {
-    document.querySelectorAll('.layer').forEach(layer => {
-        const speed = layer.getAttribute('data-speed')
+	document.querySelectorAll('.layer').forEach((layer) => {
+		const speed = layer.getAttribute('data-speed');
 
-        const x = (window.innerWidth - e.pageX * speed) / 100;
-        const y = (window.innerHeight - e.pageY * speed) / 100;
+		const x = (window.innerWidth - e.pageX * speed) / 100;
+		const y = (window.innerHeight - e.pageY * speed) / 100;
 
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-    })
-}
+		layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+	});
+};
 
-document.addEventListener("mousemove", parallax);
-
+document.addEventListener('mousemove', parallax);
 
 // CARDS PARALAX
 
 class parallaxTiltEffect {
-    constructor(element) {
-        this.element = element;
-        this.container = this.element.querySelector(".card__body");
-        this.size = [520, 260];
-        [this.w, this.h] = this.size;
+	constructor(element) {
+		this.element = element;
+		this.container = this.element.querySelector('.card__body');
+		this.size = [520, 260];
+		[this.w, this.h] = this.size;
 
-        
-        this.mouseOnComponent = false;
-        this.handleMouseMove = this.handleMouseMove.bind(this);
-        this.handleMouseEnter = this.handleMouseEnter.bind(this);
-        this.handleMouseLeave = this.handleMouseLeave.bind(this);
-        this.defaultStates = this.defaultStates.bind(this);
-        this.setProperty = this.setProperty.bind(this);
-        this.init = this.init.bind(this);
-        this.init();
-    }
+		this.mouseOnComponent = false;
+		this.handleMouseMove = this.handleMouseMove.bind(this);
+		this.handleMouseEnter = this.handleMouseEnter.bind(this);
+		this.handleMouseLeave = this.handleMouseLeave.bind(this);
+		this.defaultStates = this.defaultStates.bind(this);
+		this.setProperty = this.setProperty.bind(this);
+		this.init = this.init.bind(this);
+		this.init();
+	}
 
-    handleMouseMove(event) {
-        const {offsetX, offsetY} = event;
-        let X;
-        let Y;
+	handleMouseMove(event) {
+		if (event.target.closest('.project-link')) {
+			return;
+		}
+		const { offsetX, offsetY } = event;
+		let X;
+		let Y;
 
-        X = ((offsetX - (this.w/2)) / 3) / 4;
-        Y = (-(offsetY - (this.h/2)) / 3) / 3;
+		X = (offsetX - this.w / 2) / 3 / 4;
+		Y = -(offsetY - this.h / 2) / 3 / 3;
 
-        this.setProperty('--rY', X.toFixed(2));
-        this.setProperty('--rX', Y.toFixed(2));
-    }
+		this.setProperty('--rY', X.toFixed(2));
+		this.setProperty('--rX', Y.toFixed(2));
+	}
 
-    handleMouseEnter() {
-        this.mouseOnComponent = true;
-        this.container.classList.add("card__body--active");
-    }
+	handleMouseEnter() {
+		this.mouseOnComponent = true;
+		this.container.classList.add('card__body--active');
+	}
 
-    handleMouseLeave() {
-        this.mouseOnComponent = false;
-        this.defaultStates();
-    }
+	handleMouseLeave() {
+		this.mouseOnComponent = false;
+		this.defaultStates();
+	}
 
-    defaultStates() {
-        this.container.classList.remove("card__body--active");
-        this.setProperty('--rY', 0);
-        this.setProperty('--rX', 0);
-    }
+	defaultStates() {
+		this.container.classList.remove('card__body--active');
+		this.setProperty('--rY', 0);
+		this.setProperty('--rX', 0);
+	}
 
-    setProperty (p, v) {
-        return this.container.style.setProperty(p, v);
-    }
+	setProperty(p, v) {
+		return this.container.style.setProperty(p, v);
+	}
 
-    init() {
-        this.element.addEventListener('mousemove', this.handleMouseMove);
-        this.element.addEventListener('mouseenter', this.handleMouseEnter);
-        this.element.addEventListener('mouseleave', this.handleMouseLeave);
-    }    
+	init() {
+		this.element.addEventListener('mousemove', this.handleMouseMove);
+		this.element.addEventListener('mouseenter', this.handleMouseEnter);
+		this.element.addEventListener('mouseleave', this.handleMouseLeave);
+	}
 }
 
 const elem = document.querySelectorAll('.card-wrap');
 
 for (let i of elem) {
-    new parallaxTiltEffect(i)
+	new parallaxTiltEffect(i);
 }
-
 
 // Progress bar animation
 
@@ -87,27 +85,27 @@ const skillsSection = document.getElementById('skills-section');
 const progressBars = document.querySelectorAll('.progress-bar');
 
 const showProgress = () => {
-    progressBars.forEach(progressBar => {
-        const value = progressBar.dataset.progress;
-        progressBar.style.opacity = 1;
-        progressBar.style.width = `${value}%`;
-    });
-}
+	progressBars.forEach((progressBar) => {
+		const value = progressBar.dataset.progress;
+		progressBar.style.opacity = 1;
+		progressBar.style.width = `${value}%`;
+	});
+};
 
 const hideProgress = () => {
-    progressBars.forEach(p => {
-        p.style.opacity = 0;
-        p.style.width = 0;
-    });
-}
+	progressBars.forEach((p) => {
+		p.style.opacity = 0;
+		p.style.width = 0;
+	});
+};
 
 window.addEventListener('scroll', () => {
-    const sectionPos = skillsSection.getBoundingClientRect().top * 2;
-    const screenPos = window.innerHeight;
+	const sectionPos = skillsSection.getBoundingClientRect().top * 2;
+	const screenPos = window.innerHeight;
 
-    if(sectionPos < screenPos) {
-        showProgress();
-    } else if ((sectionPos / 2) > screenPos) {
-        hideProgress()
-    };
-})
+	if (sectionPos < screenPos) {
+		showProgress();
+	} else if (sectionPos / 2 > screenPos) {
+		hideProgress();
+	}
+});
